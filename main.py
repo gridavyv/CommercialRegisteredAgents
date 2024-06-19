686dafa9bf887892f4972c1299f1cf357d8cfb1a
Grigory Davydov <gdavidov22@gmail.com>
2024-06-18 19:14:02 -0700

    main update

--- main.py
+++ main.py
@@ -30,10 +30,23 @@
 #             count += 1
 # print(count)
 
-with open("/Users/gridavyv/Python_projects/CommercialRegisteredAgents/filtered_phones.txt") as f, \
-     open("/Users/gridavyv/Python_projects/CommercialRegisteredAgents/filtered_phones_v1.txt", 'w') as out_f:
+# with open("/Users/gridavyv/Python_projects/CommercialRegisteredAgents/filtered_phones.txt") as f, \
+#      open("/Users/gridavyv/Python_projects/CommercialRegisteredAgents/filtered_phones_v1.txt", 'w') as out_f:
+#     lines = f.readlines()
+#     for line in lines:
+#         part_1 = line.split(" ")[1].strip("\n")
+#         part_2 = line.split(" ")[2].strip("\n")
+#         out_f.write(part_1 + " " + part_2 + "\n")
+
+
+with open("/Users/gridavyv/Python_projects/CommercialRegisteredAgents/filtered_emails.txt") as f, \
+     open("/Users/gridavyv/Python_projects/CommercialRegisteredAgents/filtered_emails_v1.txt", 'w') as out_f:
     lines = f.readlines()
+    var = ""
     for line in lines:
-        part_1 = line.split(" ")[1].strip("\n")
-        part_2 = line.split(" ")[2].strip("\n")
-        out_f.write(part_1 + " " + part_2 + "\n")
+        line_list = line.split(" ")
+        if len(line_list) == 1:
+            var = "no_email"
+        else:
+            var = line_list[-1].strip("/n")
+        out_f.write(var)
